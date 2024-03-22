0ce91dc90d455145ae1ad78ad21be0877378224e
Chotifz <chotife@gmail.com>
2024-03-22 10:46:24 +0700

    fiture: add home and rooms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./src/*.{html,js}",
+  './public/**/*.{html,js}',
+],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'logo': ["MonteCarlo", "cursive", "sans-serif"],
+      
+      },
+      backgroundImage: {
+        'hero-pattern': "url(/public/img/bedroom-002.jpg)",
+        'hero-dua' : "url(/public/img/window-003.jpg)"
+
+      }
+    },
   },
   plugins: [],
 }
